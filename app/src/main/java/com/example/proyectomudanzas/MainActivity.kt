ccba2f86cb42f866948ec411a10569aa1f9a505d
AlbertoVS <albertdiable9@gmail.com>
2022-12-20 19:34:33 +0100

    Añadid0 Formulario Login

--- app/src/main/java/com/example/proyectomudanzas/MainActivity.kt
+++ app/src/main/java/com/example/proyectomudanzas/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.proyectomudanzas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.proyectomudanzas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Glide.with(this)
+            .load("https://fedem.es/wp-content/uploads/2017/02/mudanzas-fedem-logo-empresa-mudanzas-sevilla-madrid.jpg")
+            .centerCrop()
+            .into(binding.imgLogin)
     }
 }
